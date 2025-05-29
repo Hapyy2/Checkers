3e191bac8ade7fda9f97b0f9b79e464e42d00e81
Mateusz <mateusz.klemann@wp.pl>
2025-05-29 16:42:23 +0200

    kubernetes

--- api/gateway/server.js
+++ api/gateway/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:30000"],
   })
 );
 
--- frontend/src/lib/keycloak.js
+++ frontend/src/lib/keycloak.js
@@ -5,9 +5,9 @@ let keycloakInstance = null;
 const getKeycloakInstance = () => {
   if (typeof window !== "undefined" && !keycloakInstance) {
     keycloakInstance = new Keycloak({
-      url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "http://localhost:8080",
-      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "your-realm",
-      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || "your-client-id",
+      url: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
+      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
+      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
     });
   }
   return keycloakInstance;
